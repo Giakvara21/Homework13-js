0f5eb98f846c6e69ec76cf59e925573f1dd17594
Giakvara21 <kvaracxelia.giorgi52@gmail.com>
2024-04-25 00:07:12 +0400

    added script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+//homework-1:  შევქმნათ ყველა მარტივი ტიპის ცვლადი რაც ვისწავლეთ
+
+let userName = "giak";
+let age = 26;
+let isStudent = true;
+console.log("Hello, I am " + userName + " and I am " + age + " years old.");
+
+//homework-2: არითმეტიკული ოპერაციები მიმატება, გამოკლება, გაყოფა, გამრავლება ყველაფერი რაც ვისწავლეთ
+
+let x = 10;
+let y = 3;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+//homework-3: სტრინგების კონკატენაცია + ით და სტრინგების კონკატენაცია `ბექტიკებით`
+
+let job = "student";
+let at = "smart academy";
+let fullDescription = "Hello, I am a" + job + " " + "at" + " " + at;
+console.log(fullDescription);
+let subject = "java script";
+let homeWork = `learning ${subject}`;
+console.log(homeWork);
+
+//homework-4: მოცემულა მართკუთხედი სიგრძე ცვლადი = 7 და სიგანე = 5 (შექმენით 2 ცვლადი) გამოთვალეთ მართკუთხედის ფართობი ამ 2 ცვლადის გამოყენებით
+
+let length = 7;
+let width = 5;
+let area = length * width;
+console.log(area);
